e565689d9db01d342761bee3e8a4aefc66cef9c2
AndreiArlan <116577440+AndreiArlan@users.noreply.github.com>
2022-10-25 07:54:04 +0800

    Add files via upload

--- BloodData.java
+++ BloodData.java
@@ -1,28 +1,16 @@
 class BloodData {
 
-	private String bloodType;
-	private String rhFactor;
+	static String bloodType;
+	static String rhFactor;
 
 public BloodData(){
 	this.bloodType = "O";
 	this.rhFactor = "+";
 }
-public void setBloodType(String newBType){
-this.bloodType = newBType;
+public BloodData(String bt, String rh){
+ 	this.bloodType = bt;
+	this.rhFactor =  rh;
 }
-public void setRhFactor(String rh){
-this.rhFactor = rh;
-}
-public String getBloodType(){
-return bloodType;
-}
-	public String rhFactor(){
-return bloodType;
-}
-public String getRhFactor(){
-return rhFactor;
-}
-
 public void display(){
 	System.out.println(bloodType + rhFactor + " is added to the Blood Bank.");
 	}
