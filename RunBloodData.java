2e1e2abf08a7c6f4fe15f2f66d9a069337870f70
AndreiArlan <116577440+AndreiArlan@users.noreply.github.com>
2022-10-25 07:34:08 +0800

    Add files via upload

---
+++ RunBloodData.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class RunBloodData {
+	
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the Blood type of Patient: ");
+	String input1 = sc.nextLine();
+	
+	System.out.print("Enter the Rhesus Factor (+ or -): ");
+	String input2 = sc.nextLine();	
+	
+	if(input1.isEmpty()){
+	BloodData bd1 = new BloodData();
+	bd1.display();
+	}
+	else if((input1.equals("A")||input1.equals("B")||input1.equals("AB")||input1.equals("C")||input1.equals("O"))&&(input2.equals("+")||input2.equals("-"))) {
+		
+	BloodData bd2 = new BloodData(input1, input2);
+	bd2.display();
+}
+else {
+	System.out.println("Invalid Blood Type.");
+}
+}
+}
+		
+	
+		
+		
+	
+	
+	
